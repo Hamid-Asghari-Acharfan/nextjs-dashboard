2dbc2656da79da3508ffec8402190a6bff36f63c
Hamid-Asghari-nuxt <155542159+Hamid-Asghari-nuxt@users.noreply.github.com>
2024-03-13 15:08:23 +0330

    Upload simple PWA project on Github

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana,Amiri } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+    subsets: ['latin'], 
+    weight: ['400','700'] 
+});
